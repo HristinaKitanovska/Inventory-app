138836fde13b9ab2f180eebea888637aa426c7ac
HristinaKitanovska <hristina_91@hotmail.com>
2024-10-06 17:11:09 +0200

    made functionality of log out button

--- frontend/src/components/SidebarContent/SidebarContent.js
+++ frontend/src/components/SidebarContent/SidebarContent.js
@@ -1,11 +1,16 @@
 import "./SidebarContent.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "../../utils/AuthContext";
 import dashboard from "../../assets/icons/dashboard.svg";
 import inventory from "../../assets/icons/inventory.svg";
 import reports from "../../assets/icons/reports.svg";
 import signOut from "../../assets/icons/sign-out.svg";
 
 const SidebarContent = () => {
+  const { logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
   const sidebarLinks = [
     { label: "Dashboard", imageSrc: dashboard, path: "/dashboard" },
     { label: "Inventory", imageSrc: inventory, path: "/inventory" },
@@ -23,11 +28,17 @@ const SidebarContent = () => {
       </div>
     ));
   };
+
+  const handleLogout = () => {
+    logout(); // Call logout from context
+    navigate("/"); // Redirect to login page after logout
+  };
+
   return (
     <div className="sidebar-content">
       <div>{generateSidebarLinks()}</div>
-      <div className="sidebar-link">
-        <img src={signOut} />
+      <div className="sidebar-link" onClick={handleLogout}>
+        <img src={signOut} alt="" />
         <p>Sign Out</p>
       </div>
     </div>
